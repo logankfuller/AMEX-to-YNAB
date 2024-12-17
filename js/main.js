0f482d635b85889e0e9079a4b9d8fa3dfccc040d
Logan Fuller <logankfuller@gmail.com>
2024-12-16 21:46:40 -0800

    Use Prettier for formatting

--- js/main.js
+++ js/main.js
@@ -3,82 +3,84 @@ const fileInput = document.getElementById("fileInput");
 
 dropZone.addEventListener("dragover", dragoverFile);
 function dragoverFile(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (!event.dataTransfer.types.includes("Files")) {
-        return;
-    }
+  if (!event.dataTransfer.types.includes("Files")) {
+    return;
+  }
 }
 
 dropZone.addEventListener("dragenter", dragenterFile);
 function dragenterFile(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
 
 dropZone.addEventListener("drop", dropFile);
 function dropFile(event) {
-    event.preventDefault();
-    convertFile(event);
+  event.preventDefault();
+  convertFile(event);
 }
 
 fileInput.addEventListener("change", changeFile);
 function changeFile(event) {
-    convertFile(event);
+  convertFile(event);
 }
 
 async function convertFile(event) {
-    const files = event.target.files ? event.target.files : event.dataTransfer.files;
+  const files = event.target.files
+    ? event.target.files
+    : event.dataTransfer.files;
 
-    if (files.length === 0) {
-        return;
-    }
+  if (files.length === 0) {
+    return;
+  }
 
-    const file = files[0];
+  const file = files[0];
 
-    if (file.type !== "application/vnd.ms-excel") {
-        return;
-    }
+  if (file.type !== "application/vnd.ms-excel") {
+    return;
+  }
 
-    const buffer = await file.arrayBuffer();
-    const workbook = XLSX.read(buffer)
-    const worksheet = workbook.Sheets["Summary"];
+  const buffer = await file.arrayBuffer();
+  const workbook = XLSX.read(buffer);
+  const worksheet = workbook.Sheets["Summary"];
 
-    let data = XLSX.utils.sheet_to_json(worksheet, { range: 12 });
+  let data = XLSX.utils.sheet_to_json(worksheet, { range: 12 });
 
-    for (const row of data) {
-        // Convert dates to ISO and remove the time(zone).
-        const date = new Date(row['Date']);
-        row['Date'] = date.toISOString().split('T')[0];
+  for (const row of data) {
+    // Convert dates to ISO and remove the time(zone).
+    const date = new Date(row["Date"]);
+    row["Date"] = date.toISOString().split("T")[0];
 
-        // Delete unnecessary columns
-        delete row['Additional Information'];
-        delete row['Date Processed'];
-        delete row['Merchant Address'];
+    // Delete unnecessary columns
+    delete row["Additional Information"];
+    delete row["Date Processed"];
+    delete row["Merchant Address"];
 
-        // Convert currency to number
-        row['Amount'] = row['Amount'].replace('$', '');
-        row['Amount'] = parseFloat(row['Amount']);
+    // Convert currency to number
+    row["Amount"] = row["Amount"].replace("$", "");
+    row["Amount"] = parseFloat(row["Amount"]);
 
-        // Swap signs to match YNAB
-        row['Amount'] = (row['Amount'] * -1.00).toFixed(2);
+    // Swap signs to match YNAB
+    row["Amount"] = (row["Amount"] * -1.0).toFixed(2);
 
-        // Rename columns to match YNAB
-        row['Payee'] = row['Merchant'];
-        delete row['Merchant'];
-        row['Memo'] = row['Description'];
-        delete row['Description'];
-    }
+    // Rename columns to match YNAB
+    row["Payee"] = row["Merchant"];
+    delete row["Merchant"];
+    row["Memo"] = row["Description"];
+    delete row["Description"];
+  }
 
-    const newWorksheet = XLSX.utils.json_to_sheet(data);
-    const newWorkbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, "Summary");
-    const csv = XLSX.write(newWorkbook, { type: "array", bookType: "csv" });
+  const newWorksheet = XLSX.utils.json_to_sheet(data);
+  const newWorkbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, "Summary");
+  const csv = XLSX.write(newWorkbook, { type: "array", bookType: "csv" });
 
-    const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
 
-    // Update download button and show it
-    const downloadButton = document.querySelector("#downloadLink");
-    downloadButton.href = URL.createObjectURL(blob);
-    downloadButton.download = "ConvertedTransactions.csv";
-    downloadButton.style.display = "block";
+  // Update download button and show it
+  const downloadButton = document.querySelector("#downloadLink");
+  downloadButton.href = URL.createObjectURL(blob);
+  downloadButton.download = "ConvertedTransactions.csv";
+  downloadButton.style.display = "block";
 }
